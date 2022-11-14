b83abf49380237b206e8e91a6e0fd9c8f7a52213
dastanchick <dosyafree32@gmail.com>
2022-11-14 16:25:59 +0600

    I added 6-13 line

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int one = scanner.nextInt();
+      minus(one);
     }
+    public static void minus(int a){
+        int []array ={a};
+        for (int i = array.length ; i <=0; i++) {
+            System.out.print(array[i]);
+
+        }
+    }
+
 }
