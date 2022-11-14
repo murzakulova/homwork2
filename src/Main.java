cc22678ad98f75e78fdc27b0c84eb8ffe161a1f2
dastanchick <dosyafree32@gmail.com>
2022-11-14 19:37:51 +0600

    I added line 30

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-a(scanner.nextInt());
-
-
-
-
-    }
-   public  static void a(int a){
-   if (a>999 || a<-999){
-       System.out.println(0);
-
-
-       }
-
-
         int one = scanner.nextInt();
-      minus(one);
+        int two;
+        a(one);
+        two = scanner.nextInt();
+        a(two);
     }
-    public static void minus(int a){
-     int one = (a/100)%10;
-     int two = (a/10)%10;
-     int three = a%10;
-        System.out.println(three+""+two+""+one);
+    public static void a(int a) {
+        if (a > 999 || a < -999) {
+            System.out.println(0);
+        } else if (a<0) {
+            String str = String.valueOf(a);
+            char[]win = new char[a];
+            for (int i = win.length -1; i >=0 ; i--) {
+                System.out.println(win[i]);
+            }
+    } else {
+        int one = (a / 100) % 10;
+        int two = (a / 10) % 10;
+        int three = a % 10;
+        System.out.println(three + "" + two + "" + one);
+        }
     }
 
-       }
-}
+
+         }
+
+
