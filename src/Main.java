5743a30b4b7e9b899ad68c2c4fe4aa49cf7c8f5a
murzakulova <aikenm310@gmail.com>
2022-11-14 16:55:37 +0600

    ssssss

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+a(scanner.nextInt());
+
+
+
 
     }
+   public  static void a(int a){
+   if (a>999 || a<-999){
+       System.out.println(0);
+
+
+       }
+
+
+
+       }
 }
